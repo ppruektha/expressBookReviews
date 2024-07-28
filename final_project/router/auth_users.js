dbbcc23eb83cdfd1ba3759c85a69d15f953f0d25
Phaisit Pruektharathikul <ppruek@Phaisits-MacBook-Air.local>
2024-07-29 06:53:09 +0800

    Step 10

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -61,13 +61,32 @@ regd_users.post("/login", (req,res) => {
   });
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  const review = req.body.review;
-  books[isbn].reviews = review;
-  return res.status(300).json({message: 'The review for the book with ISBN' + isbn + "has been added/updated"});
+  
+      const user = req.session.authorization["username"]
+      const isbn = req.params.isbn;
+      const review = req.body.review;
+      books[isbn]["reviews"][user] = review;
+
+      return res.status(403).json(books);
+
 });
 
 
+regd_users.delete("/auth/delete/:isbn", (req, res) => {
+  if(req.session.authorization){
+      const user = req.session.authorization["username"]
+      const isbn = req.params.isbn;
+      if(Object.keys(books[isbn]["reviews"]).includes(user)){
+          delete books[isbn]["reviews"][user];
+          return res.status(200).json({ message: "Review deleted successfully" });
+      }else{
+          return res.status(200).json({ message: "User haven't posted any reviews yet" });
+      }  
+  }else{
+      return res.status(403).json({ message: "User not logged in" });
+  }
+
+});
 
 module.exports.authenticated = regd_users;
 module.exports.isValid = isValid;
